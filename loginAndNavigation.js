e1ebb72a83d155817703624714005a3094d89490
Fran Rouco <fran.rouco@metadrop.net>
2024-10-21 14:23:05 +0200

    Improved page-ready detection on slow networks

--- loginAndNavigation.js
+++ loginAndNavigation.js
@@ -9,11 +9,11 @@ module.exports = async(page, scenario, config) => {
       await page.type(scenario.loginWrapperSelector + ' input[name="name"]', user);
       await page.type(scenario.loginWrapperSelector + ' input[name="pass"]', pass);
       await page.keyboard.press('Enter'); // Enter Key
-      await page.waitForNavigation({ waitUntil: 'networkidle0'});
+      await page.waitForNavigation({ waitUntil: ['load', 'networkidle0']});
 
       if (scenario.loginRedirectTo) {
         await page.goto(scenario.loginRedirectTo, {
-          waitUntil: 'networkidle0'
+          waitUntil: ['load', 'networkidle0']
         });
       }
 
